d62cfdbfe54de0191cad2e683dfe4686676c8a6a
unknown <cmumford@PC0305.ad.mackaysstores.co.uk>
2015-02-25 15:35:10 +0000

    Adding solution to hold all custom MSBuild tasks.

---
+++ CustomTask/MSBuildCustomTasks/MSBuildCustomTasks/AddTwoNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace MSBuildCustomTasks
+{
+    using Microsoft.Build.Utilities;
+    using Microsoft.Build.Framework;
+
+    public class AddTwoNumbers : Task
+    {
+        [Required]
+        public double Number1 { get; set; }
+
+        [Required]
+        public double Number2 { get; set; }
+
+        [Output]
+        public double Result { get; set; }
+
+        public override bool Execute()
+        {
+            Result = Number1 + Number2;
+
+            Log.LogMessage(MessageImportance.High, "Add two numbers", null);
+
+            return true;
+        }
+    }
+}
